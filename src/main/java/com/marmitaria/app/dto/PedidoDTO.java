4b0c6aa1622531c515de0099c0ad93c572dd2a13
Lucas Souza <lucas-machado@outlook.com>
2018-10-23 22:14:29 -0300

    Merge com versão mais atual do projeto.

--- src/main/java/com/marmitaria/app/dto/PedidoDTO.java
+++ src/main/java/com/marmitaria/app/dto/PedidoDTO.java
@@ -1,5 +1,7 @@
 package com.marmitaria.app.dto;
 
+import java.util.List;
+
 import com.marmitaria.app.domain.Pedido;
 
 public class PedidoDTO {
@@ -7,46 +9,16 @@ public class PedidoDTO {
 	private Long id;
 	private int quantidade;
 	private String nomeDoCliente;
-	private String prato;
+	private List<String> pratos;
 	
 	public PedidoDTO(Pedido p) {
 		super();
 		this.id = p.getId();
 		this.quantidade = p.getQuantidade();
 		this.nomeDoCliente = p.getCliente().getNome();
-		this.prato = p.getPrato().getNome();
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public int getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
-	}
-
-	public String getNomeDoCliente() {
-		return nomeDoCliente;
-	}
-
-	public void setNomeDoCliente(String nomeDoCliente) {
-		this.nomeDoCliente = nomeDoCliente;
-	}
-
-	public String getPrato() {
-		return prato;
-	}
-
-	public void setPrato(String prato) {
-		this.prato = prato;
+		p.getPratos().forEach(i -> {
+			pratos.add(i.getNome());
+		});
 	}
 		
 }
