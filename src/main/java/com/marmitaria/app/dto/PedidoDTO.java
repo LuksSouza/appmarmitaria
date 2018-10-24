90091b3f0727f8505357f96a0e2d247f4f786f89
Lucas Souza <lucas-machado@outlook.com>
2018-10-23 22:40:12 -0300

    Criado DTO para a entidade Prato, pois agora o pedido pode retornar uma lista de Prato. Então, foi criado um DTO de Prato para exibir no JSON de retorno apenas o nome do prato do pedido.

--- src/main/java/com/marmitaria/app/dto/PedidoDTO.java
+++ src/main/java/com/marmitaria/app/dto/PedidoDTO.java
@@ -1,24 +1,59 @@
 package com.marmitaria.app.dto;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.marmitaria.app.domain.Pedido;
+import com.marmitaria.app.domain.Prato;
 
-public class PedidoDTO {
+public class PedidoDTO implements Serializable {
 
 	private Long id;
 	private int quantidade;
 	private String nomeDoCliente;
-	private List<String> pratos;
+	private List<PratoDTO> pratos = new ArrayList<>();
 	
 	public PedidoDTO(Pedido p) {
 		super();
 		this.id = p.getId();
 		this.quantidade = p.getQuantidade();
 		this.nomeDoCliente = p.getCliente().getNome();
-		p.getPratos().forEach(i -> {
-			pratos.add(i.getNome());
-		});
+		for (Prato prato : p.getPratos()) {
+			pratos.add(new PratoDTO(prato));
+		}
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getNomeDoCliente() {
+		return nomeDoCliente;
+	}
+
+	public void setNomeDoCliente(String nomeDoCliente) {
+		this.nomeDoCliente = nomeDoCliente;
+	}
+
+	public List<PratoDTO> getPratos() {
+		return pratos;
+	}
+
+	public void setPratos(List<PratoDTO> pratos) {
+		this.pratos = pratos;
 	}
 		
 }
---
+++ src/main/java/com/marmitaria/app/dto/PratoDTO.java
@@ -0,0 +1,24 @@
+package com.marmitaria.app.dto;
+
+import java.io.Serializable;
+
+import com.marmitaria.app.domain.Prato;
+
+public class PratoDTO implements Serializable {
+
+	private String nomeDoPrato;
+	
+	public PratoDTO(Prato p) {
+		super();
+		this.nomeDoPrato = p.getNome();
+	}
+
+	public String getNomeDoPrato() {
+		return nomeDoPrato;
+	}
+
+	public void setNomeDoPrato(String nomeDoPrato) {
+		this.nomeDoPrato = nomeDoPrato;
+	}
+	
+}
